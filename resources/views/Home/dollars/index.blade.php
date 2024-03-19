f6dc7f212b62338b3f5b73c7779015f34f45b714
Arturo Ruiz <42457341+Arturo-Ruiz@users.noreply.github.com>
2024-03-18 20:03:17 -0400

    remove

--- resources/views/Home/dollars/index.blade.php
+++ resources/views/Home/dollars/index.blade.php
@@ -55,6 +55,14 @@
         </nav>
     </header>
 
+    <section class="anuncio_collapse">
+        <div class="sidebar">
+            <p class="text-center h6 text-primary text-uppercase fw-bold">Prueba nuestro buscador</p>
+            <p class=" mx-4 text-secondary">Haz tu búsqueda por alfabeto, nombre o componente activo</p>
+        </div>
+
+    </section>
+
 
     <!-- carousel intro  -->
 
--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -59,6 +59,15 @@
     </header>
 
 
+    <section class="anuncio_collapse">
+        <div class="sidebar">
+            <p class="text-center h6 text-primary text-uppercase fw-bold">Prueba nuestro buscador</p>
+            <p class=" mx-4 text-secondary">Haz tu búsqueda por alfabeto, nombre o componente activo</p>
+        </div>
+
+     
+    </section>
+
 
     <!-- carousel intro  -->
 
