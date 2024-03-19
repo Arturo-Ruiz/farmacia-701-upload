7a7bb90e2f45bfb9299c2e2b9728fdf8179d59f5
Arturo Ruiz <42457341+Arturo-Ruiz@users.noreply.github.com>
2024-03-18 23:26:34 -0400

    update panel controler

--- app/Http/Controllers/Admin/PanelController.php
+++ app/Http/Controllers/Admin/PanelController.php
@@ -11,18 +11,18 @@ class PanelController extends Controller
 {
     public function index(){
 
-        $enero = Sale::where('created_at', 'LIKE', "%2023-01%")->get()->count();
-        $febrero = Sale::where('created_at', 'LIKE', "%2023-02%")->get()->count();
-        $marzo = Sale::where('created_at', 'LIKE', "%2023-03%")->get()->count();
-        $abril = Sale::where('created_at', 'LIKE', "%2023-04%")->get()->count();
-        $mayo = Sale::where('created_at', 'LIKE', "%2023-05%")->get()->count();
-        $junio = Sale::where('created_at', 'LIKE', "%2023-06%")->get()->count();
-        $julio = Sale::where('created_at', 'LIKE', "%2023-07%")->get()->count();
-        $agosto = Sale::where('created_at', 'LIKE', "%2023-08%")->get()->count();
-        $septiembre = Sale::where('created_at', 'LIKE', "%2023-09%")->get()->count();
-        $octubre = Sale::where('created_at', 'LIKE', "%2023-10%")->get()->count();
-        $noviembre = Sale::where('created_at', 'LIKE', "%2023-11%")->get()->count();
-        $diciembre = Sale::where('created_at', 'LIKE', "%2023-12%")->get()->count();
+        $enero = Sale::where('created_at', 'LIKE', "%2024-01%")->get()->count();
+        $febrero = Sale::where('created_at', 'LIKE', "%2024-02%")->get()->count();
+        $marzo = Sale::where('created_at', 'LIKE', "%2024-03%")->get()->count();
+        $abril = Sale::where('created_at', 'LIKE', "%2024-04%")->get()->count();
+        $mayo = Sale::where('created_at', 'LIKE', "%2024-05%")->get()->count();
+        $junio = Sale::where('created_at', 'LIKE', "%2024-06%")->get()->count();
+        $julio = Sale::where('created_at', 'LIKE', "%2024-07%")->get()->count();
+        $agosto = Sale::where('created_at', 'LIKE', "%2024-08%")->get()->count();
+        $septiembre = Sale::where('created_at', 'LIKE', "%2024-09%")->get()->count();
+        $octubre = Sale::where('created_at', 'LIKE', "%2024-10%")->get()->count();
+        $noviembre = Sale::where('created_at', 'LIKE', "%2024-11%")->get()->count();
+        $diciembre = Sale::where('created_at', 'LIKE', "%2024-12%")->get()->count();
 
         return view('admin.panel.index', compact('enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'));
     }
