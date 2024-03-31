46662e187127512de759d2d218b293c906f6d78c
Arturo Ruiz <42457341+Arturo-Ruiz@users.noreply.github.com>
2024-03-31 14:29:34 -0400

    test

--- resources/views/Home/whatsapp.blade.php
+++ resources/views/Home/whatsapp.blade.php
@@ -1,13 +1,12 @@
 <script>
-
     let name ="{{ $name }}"; 
     
-    let msg = `
-    Nombre Y Apellido o Empresa: ${name}
+    let msg = `Nombre Y Apellido o Empresa: **${name}**
+    
     `; 
 
-    let url = `https://api.whatsapp.com/send?phone=584124212799&text=${encodeURIComponent(msg)}`; 
+    // let url = `https://api.whatsapp.com/send?phone=584141850671&text=${encodeURIComponent(msg)}`; 
     
-    location.href = url;
+    // location.href = url;
 
 </script>
