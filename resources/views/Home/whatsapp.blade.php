63f44cffc26a905741dfac401677ec7079e1fba3
Arturo Ruiz <42457341+Arturo-Ruiz@users.noreply.github.com>
2024-03-31 14:26:59 -0400

    sasas

--- resources/views/Home/whatsapp.blade.php
+++ resources/views/Home/whatsapp.blade.php
@@ -1,9 +1,13 @@
 <script>
+
     let name ="{{ $name }}"; 
+    
     let msg = `
     Nombre Y Apellido o Empresa: ${name}
     `; 
 
     let url = `https://api.whatsapp.com/send?phone=584124212799&text=${encodeURIComponent(msg)}`; 
+    
     location.href = url;
+
 </script>
