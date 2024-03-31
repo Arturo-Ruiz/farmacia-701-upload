56fd71a4765960c3d6dd1b64263733b843b77d79
Arturo Ruiz <42457341+Arturo-Ruiz@users.noreply.github.com>
2024-03-31 14:15:03 -0400

    ssa

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -140,7 +140,7 @@ class WebController extends Controller
             $msg_productos = " " . $msg_productos . "" . $product->nombre . ", Cantidad: " . $product->cantidad . " ; ";
         }
 
-        $msg = "*Nombre Y Apellido O Empresa* : " . $request->name . ", *Cedula o RIF* : " . $request->dni . ", *Email* : " . $request->email . ", *Teléfono* : " . $request->phone . ", *Dirección* : " . $request->address . ", *Método de entrega* : " . $request->metodo_entrega . ", *Método de pago* : " . $request->paymentMethod . " " . $msg_productos . ", *Total* : " . $request->total . " .Bs, Y Los productos que me gustaria que tuvieran disponibles son : " . $request->solicitud;
+        $msg = "*Nombre* : " . $request->name . ", *Email* : " . $request->email . ", *Teléfono* : " . $request->phone . ", *Dirección* : " . $request->address . ", *Método de entrega* : " . $request->metodo_entrega . ", *Método de pago* : " . $request->paymentMethod . " " . $msg_productos . ", *Total* : " . $request->total . " .Bs, Y Los productos que me gustaria que tuvieran disponibles son : " . $request->solicitud;
 
 
         return view("Home.whatsapp", compact("msg"));
