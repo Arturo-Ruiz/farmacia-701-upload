4c011f0b4e0973c7f1f25f450692ae539a82d840
Arturo Ruiz <42457341+Arturo-Ruiz@users.noreply.github.com>
2024-03-31 22:33:04 -0400

    working in bss

--- resources/views/Home/whatsapp.blade.php
+++ resources/views/Home/whatsapp.blade.php
@@ -69,8 +69,7 @@ if (requestProducts) {
 
 Y Los productos que me gustaría que tuvieran disponibles son: *${requestProducts}*`
      }
-     let url = `https://api.whatsapp.com/send?phone=584124212799&text=${encodeURIComponent(msg)}`; 
 
-    // let url = `https://api.whatsapp.com/send?phone=584141850671&text=${encodeURIComponent(msg)}`; 
+    let url = `https://api.whatsapp.com/send?phone=584141850671&text=${encodeURIComponent(msg)}`; 
     location.href = url;
 </script>
