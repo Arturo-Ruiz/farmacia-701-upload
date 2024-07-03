237c9746cf8d2de27171ea354edc5438a08fa85a
Arturo Ruiz <Gregory.Arturo.Ruiz@gmail.com>
2024-07-03 13:43:36 -0400

    testing

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -20,6 +20,7 @@ class ProductsImport implements ToModel
         }else{
             $check = false;
         }
+        dd($row)
         return new Product([
             'name'     => $row[1],
             'category_id'     => $row[2],
